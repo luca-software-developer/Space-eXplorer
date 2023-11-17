edd442c04b50d3e7d34af4b12d81c46e594e5ea9
Luca Dello Russo <lucadellorusso5@gmail.com>
2023-11-17 09:24:09 +0100

    Refactoring & Ottimizzazione

---
+++ js/script.js
@@ -0,0 +1,527 @@
+/**
+ * Space eXplorer Game
+ * SpaceGamersHub Project
+ * 
+ * @version 1.0.0.0
+ * @author Software Dev Team
+ */
+
+/* Canvas, Context & GAME OVER Overlay */
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d", { willReadFrequently: true });
+const gameOverOverlay = document.getElementById('gameover-overlay');
+
+/* GameObjects */
+const spaceship = document.getElementById('spaceship');
+const asteroid = document.getElementById('asteroid');
+const explosion = document.getElementById('explosion');
+const enemy = document.getElementById('enemy');
+const playerBullet = document.getElementById('playerbullet');
+const enemyBullet = document.getElementById('enemybullet');
+
+/* Scene Background Color */
+const BACKGROUND_COLOR = "#010b19";
+
+/* Soundtrack & SFX */
+const EXPLOSION_SFX_PATH = "audio/explosion.mp3";
+const GAMEOVER_SFX_PATH = "audio/gameover.mp3";
+const SCORE_SFX_PATH = "audio/score.mp3";
+const SOUNDTRACK_PATH = "audio/soundtrack.mp3";
+
+/* Player Constants */
+const PLAYER_WIDTH = 300;
+const PLAYER_HEIGHT = 92;
+const PLAYER_FRAMES = 65;
+
+/* Asteroid Constants */
+const ASTEROID_WIDTH = 100;
+const ASTEROID_HEIGHT = 100;
+const ASTEROID_FRAMES = 245;
+
+/* Explosion Constants */
+const EXPLOSION_WIDTH = 800;
+const EXPLOSION_HEIGHT = 600;
+const EXPLOSION_FRAMES = 60;
+
+/* Enemy Constants */
+const ENEMY_WIDTH = 150;
+const ENEMY_HEIGHT = 72;
+
+/* PlayerBullet Constants */
+const PLAYERBULLET_WIDTH = 50;
+const PLAYERBULLET_HEIGHT = 29;
+
+/* EnemyBullet Constants */
+const ENEMYBULLET_WIDTH = 50;
+const ENEMYBULLET_HEIGHT = 29;
+
+/* Canvas Sizing */
+let CANVAS_WIDTH = innerWidth;
+let CANVAS_HEIGHT = innerHeight;
+
+/* Initial Player Positioning */
+let x = CANVAS_WIDTH / 3 - PLAYER_WIDTH / 2;
+let y = CANVAS_HEIGHT / 2 - PLAYER_HEIGHT / 2;
+
+/* Game Variables */
+let keys = [];
+let score = 0;
+let imageData = null;
+let isRunning = false;
+
+/* Score Variables */
+let remainingScoreBlinks = 0;
+let isScoreVisible = true;
+let lastScoreBlink = Date.now();
+
+/* Player Variables */
+let playerFrame = 0;
+let isPlayerAlive = true;
+let lastPlayerFrame = Date.now();
+
+/* Asteroid Variables */
+let asteroidsX = [];
+let asteroidsY = [];
+let asteroidFrame = 0;
+let lastAsteroid = Date.now();
+let lastAsteroidFrame = Date.now();
+
+/* Enemy Variables */
+let enemiesX = [];
+let enemiesY = [];
+let lastEnemy = Date.now();
+
+/* Explosion Variables */
+let explosionsX = [];
+let explosionsY = [];
+let explosionsFrames = [];
+
+/* PlayerBullet Variables */
+let playerBulletsX = [];
+let playerBulletsY = [];
+
+/* EnemyBullet Variables */
+let enemyBulletsX = [];
+let enemyBulletsY = [];
+let lastEnemyBullets = [];
+
+/* Canvas Initial Sizing */
+canvas.setAttribute('width', innerWidth);
+canvas.setAttribute('height', innerHeight);
+
+/* Clear Canvas */
+const clear = () => {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+/* Canvas Dynamic Resizing */
+document.onresize = () => {
+    canvas.setAttribute('width', innerWidth);
+    canvas.setAttribute('height', innerHeight);
+    CANVAS_WIDTH = innerWidth;
+    CANVAS_HEIGHT = innerHeight;
+    x = CANVAS_WIDTH / 3 - PLAYER_WIDTH / 2;
+    y = CANVAS_HEIGHT / 2 - PLAYER_HEIGHT / 2;
+    clear();
+    imageData = null;
+    drawBackground();
+}
+
+/* Scene Background Drawing Routine (called at each frame) */
+const drawBackground = () => {
+    ctx.fillStyle = BACKGROUND_COLOR;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillStyle = "#ffffff";
+    if (imageData == null) {
+        for (let x = 0; x <= CANVAS_WIDTH; x += 2) {
+            if (Math.random() > .5) {
+                const y = Math.random() * CANVAS_HEIGHT;
+                ctx.fillStyle = `rgba(255, 255, 255, ${Math.random()})`;
+                ctx.fillRect(x, y, 2, 2);
+            }
+        }
+    } else {
+        ctx.putImageData(imageData, 0, 0);
+        ctx.fillStyle = BACKGROUND_COLOR;
+        ctx.fillRect(CANVAS_WIDTH - 2, 0, 2, CANVAS_HEIGHT);
+        ctx.fillStyle = "#ffffff";
+        if (Math.random() > .5) {
+            const y = Math.random() * CANVAS_HEIGHT;
+            ctx.fillStyle = `rgba(255, 255, 255, ${Math.random()})`;
+            ctx.fillRect(CANVAS_WIDTH - 2, y, 2, 2);
+        }
+    }
+    imageData = ctx.getImageData(2, 0, CANVAS_WIDTH - 2, CANVAS_HEIGHT);
+}
+
+/* Player Drawing Routine */
+const drawPlayer = () => {
+    ctx.drawImage(spaceship, PLAYER_WIDTH * playerFrame, 0, PLAYER_WIDTH, PLAYER_HEIGHT, x, y, PLAYER_WIDTH, PLAYER_HEIGHT);
+    if (Date.now() - lastPlayerFrame > 1000 / 15) {
+        playerFrame = (playerFrame + 1) % PLAYER_FRAMES;
+        lastPlayerFrame = Date.now();
+    }
+}
+
+/* Player Collision Routine, spawns Explosion GameObject */
+const checkForPlayerCollision = () => {
+    let isInCollision = false;
+    for (let index = 0; index < asteroidsX.length; index++) {
+        const isInHorizontalCollision = x + PLAYER_WIDTH / 2 < asteroidsX[index] + ASTEROID_WIDTH && x + PLAYER_WIDTH > asteroidsX[index];
+        const isInVerticalCollision = y < asteroidsY[index] + ASTEROID_HEIGHT && y + PLAYER_HEIGHT > asteroidsY[index];
+        isInCollision = isInHorizontalCollision && isInVerticalCollision;
+        if (isInCollision) {
+            console.log("Collisione tra PLAYER e ASTEROID");
+            asteroidsX.splice(index, 1);
+            asteroidsY.splice(index, 1);
+            spawnExplosion(x + PLAYER_WIDTH / 2, y + PLAYER_HEIGHT / 2);
+            isPlayerAlive = false;
+            new Audio(GAMEOVER_SFX_PATH).play();
+            gameOverOverlay.style.display = 'flex';
+            return isInCollision;
+        }
+    }
+    for (let index = 0; index < enemiesX.length; index++) {
+        const isInHorizontalCollision = x + PLAYER_WIDTH / 2 < enemiesX[index] + ENEMY_WIDTH && x + PLAYER_WIDTH > enemiesX[index];
+        const isInVerticalCollision = y < enemiesY[index] + ENEMY_HEIGHT && y + PLAYER_HEIGHT > enemiesY[index];
+        isInCollision = isInHorizontalCollision && isInVerticalCollision;
+        if (isInCollision) {
+            console.log("Collisione tra PLAYER e ENEMY");
+            enemiesX.splice(index, 1);
+            enemiesY.splice(index, 1);
+            spawnExplosion(x + PLAYER_WIDTH / 2, y + PLAYER_HEIGHT / 2);
+            isPlayerAlive = false;
+            new Audio(GAMEOVER_SFX_PATH).play();
+            gameOverOverlay.style.display = 'flex';
+            return isInCollision;
+        }
+    }
+    for (let index = 0; index < enemyBulletsX.length; index++) {
+        const isInHorizontalCollision = x + PLAYER_WIDTH / 2 < enemyBulletsX[index] + ENEMYBULLET_WIDTH && x + PLAYER_WIDTH > enemyBulletsX[index];
+        const isInVerticalCollision = y < enemyBulletsY[index] + ENEMYBULLET_HEIGHT && y + PLAYER_HEIGHT > enemyBulletsY[index];
+        isInCollision = isInHorizontalCollision && isInVerticalCollision;
+        if (isInCollision) {
+            console.log("Collisione tra PLAYER e ENEMYBULLET");
+            enemyBulletsX.splice(index, 1);
+            enemyBulletsY.splice(index, 1);
+            spawnExplosion(x + PLAYER_WIDTH / 2, y + PLAYER_HEIGHT / 2);
+            isPlayerAlive = false;
+            new Audio(GAMEOVER_SFX_PATH).play();
+            gameOverOverlay.style.display = 'flex';
+            return isInCollision;
+        }
+    }
+    return isInCollision;
+}
+
+/* Asteroid Drawing Routine */
+const drawAsteroid = (x, y) => {
+    ctx.drawImage(asteroid, ASTEROID_WIDTH * asteroidFrame, 0, ASTEROID_WIDTH, ASTEROID_HEIGHT, x, y, ASTEROID_WIDTH, ASTEROID_HEIGHT);
+    if (Date.now() - lastAsteroidFrame > 1000 / 15) {
+        asteroidFrame = (asteroidFrame + 1) % ASTEROID_FRAMES;
+        lastAsteroidFrame = Date.now();
+    }
+}
+
+/* Asteroids Updating Routine (called at each frame) */
+const updateAsteroids = () => {
+    for (let index = 0; index < asteroidsX.length; index++) {
+        if (asteroidsX[index] < -ASTEROID_WIDTH) {
+            asteroidsX.splice(index, 1);
+            asteroidsY.splice(index, 1);
+        }
+    }
+    for (let index = 0; index < asteroidsX.length; index++) {
+        drawAsteroid(asteroidsX[index], asteroidsY[index]);
+        asteroidsX[index] -= 3;
+    }
+}
+
+/* Asteroid Automatic Spawning Routine (called at each frame) */
+const spawnAsteroid = () => {
+    if (Date.now() - lastAsteroid > 3000) {
+        asteroidsX.push(CANVAS_WIDTH + ASTEROID_WIDTH / 2);
+        asteroidsY.push((Math.random() * (CANVAS_HEIGHT - 2 * ASTEROID_HEIGHT)) + ASTEROID_HEIGHT);
+        lastAsteroid = Date.now();
+    }
+}
+
+/* Explosion Drawing Routine */
+const drawExplosion = (x, y, explosionFrame) => {
+    x = x - EXPLOSION_WIDTH / 2;
+    y = y - EXPLOSION_HEIGHT / 2;
+    ctx.drawImage(explosion, EXPLOSION_WIDTH * explosionFrame, 0, EXPLOSION_WIDTH, EXPLOSION_HEIGHT, x, y, EXPLOSION_WIDTH, EXPLOSION_HEIGHT);
+}
+
+/* Explosions Updating Routine (called at each frame) */
+const updateExplosions = () => {
+    for (let index = 0; index < explosionsX.length; index++) {
+        drawExplosion(explosionsX[index], explosionsY[index], explosionsFrames[index]);
+        explosionsFrames[index]++;
+        if (explosionsFrames[index] == EXPLOSION_FRAMES) {
+            explosionsX.splice(index, 1);
+            explosionsY.splice(index, 1);
+            explosionsFrames.splice(index, 1);
+        }
+    }
+}
+
+/* Explosion Spawning Routine */
+const spawnExplosion = (x, y) => {
+    explosionsX.push(x);
+    explosionsY.push(y);
+    explosionsFrames.push(0);
+    new Audio(EXPLOSION_SFX_PATH).play();
+}
+
+/* Enemy Drawing Routine */
+const drawEnemy = (x, y) => {
+    ctx.drawImage(enemy, 0, 0, ENEMY_WIDTH, ENEMY_HEIGHT, x, y, ENEMY_WIDTH, ENEMY_HEIGHT);
+}
+
+/* Enemies Updating Routine (called at each frame) */
+const updateEnemies = () => {
+    for (let index = 0; index < enemiesX.length; index++) {
+        if (enemiesX[index] < -ENEMY_WIDTH) {
+            enemiesX.splice(index, 1);
+            enemiesY.splice(index, 1);
+            lastEnemyBullets.splice(index, 1);
+        }
+    }
+    for (let index = 0; index < enemiesX.length; index++) {
+        drawEnemy(enemiesX[index], enemiesY[index]);
+        enemiesX[index] -= 4;
+    }
+}
+
+/* Enemy Spawning Routine (called at each frame) */
+const spawnEnemy = () => {
+    if (Date.now() - lastEnemy > 3000) {
+        enemiesX.push(CANVAS_WIDTH + ENEMY_WIDTH / 2);
+        enemiesY.push((Math.random() * (CANVAS_HEIGHT - 2 * ENEMY_HEIGHT)) + ENEMY_HEIGHT);
+        lastEnemyBullets.push(Date.now());
+        lastEnemy = Date.now();
+    }
+}
+
+/* PlayerBullet Drawing Routine */
+const drawPlayerBullet = (x, y) => {
+    ctx.drawImage(playerBullet, 0, 0, PLAYERBULLET_WIDTH, PLAYERBULLET_HEIGHT, x, y, PLAYERBULLET_WIDTH, PLAYERBULLET_HEIGHT);
+}
+
+/* PlayerBullets Updating Routine (called at each frame) */
+const updatePlayerBullets = () => {
+    for (let index = 0; index < playerBulletsX.length; index++) {
+        if (playerBulletsX[index] > CANVAS_WIDTH + PLAYERBULLET_WIDTH) {
+            playerBulletsX.splice(index, 1);
+            playerBulletsY.splice(index, 1);
+        }
+    }
+    for (let index = 0; index < playerBulletsX.length; index++) {
+        drawPlayerBullet(playerBulletsX[index], playerBulletsY[index]);
+        playerBulletsX[index] += 6;
+    }
+}
+
+/* PlayerBullet Spawning Routine */
+const spawnPlayerBullet = () => {
+    playerBulletsX.push(x + PLAYER_WIDTH);
+    playerBulletsY.push(y + PLAYER_HEIGHT / 3);
+}
+
+/* PlayerBullet Collision Routine, spawns Explosion GameObject */
+const checkForPlayerBulletCollision = () => {
+    let isInCollision = false;
+    for (let i = 0; i < playerBulletsX.length; i++) {
+        for (let index = 0; index < asteroidsX.length; index++) {
+            const isInHorizontalCollision = playerBulletsX[i] + PLAYERBULLET_WIDTH / 2 < asteroidsX[index] + ASTEROID_WIDTH && playerBulletsX[i] + PLAYERBULLET_WIDTH > asteroidsX[index];
+            const isInVerticalCollision = playerBulletsY[i] < asteroidsY[index] + ASTEROID_HEIGHT && playerBulletsY[i] + PLAYERBULLET_HEIGHT > asteroidsY[index];
+            isInCollision = isInHorizontalCollision && isInVerticalCollision;
+            if (isInCollision) {
+                console.log("Collisione tra PLAYERBULLET e ASTEROID");
+                asteroidsX.splice(index, 1);
+                asteroidsY.splice(index, 1);
+                spawnExplosion(playerBulletsX[i], playerBulletsY[i]);
+                playerBulletsX.splice(i, 1);
+                playerBulletsY.splice(i, 1);
+                return isInCollision;
+            }
+        }
+        for (let index = 0; index < enemiesX.length; index++) {
+            const isInHorizontalCollision = playerBulletsX[i] + PLAYERBULLET_WIDTH / 2 < enemiesX[index] + ENEMY_WIDTH && playerBulletsX[i] + PLAYERBULLET_WIDTH > enemiesX[index];
+            const isInVerticalCollision = playerBulletsY[i] < enemiesY[index] + ENEMY_HEIGHT && playerBulletsY[i] + PLAYERBULLET_HEIGHT > enemiesY[index];
+            isInCollision = isInHorizontalCollision && isInVerticalCollision;
+            if (isInCollision) {
+                console.log("Collisione tra PLAYERBULLET e ENEMY");
+                enemiesX.splice(index, 1);
+                enemiesY.splice(index, 1);
+                spawnExplosion(playerBulletsX[i], playerBulletsY[i]);
+                playerBulletsX.splice(i, 1);
+                playerBulletsY.splice(i, 1);
+                return isInCollision;
+            }
+        }
+    }
+    return isInCollision;
+}
+
+/* EnemyBullet Drawing Routine */
+const drawEnemyBullet = (x, y) => {
+    ctx.drawImage(enemyBullet, 0, 0, ENEMYBULLET_WIDTH, ENEMYBULLET_HEIGHT, x, y, ENEMYBULLET_WIDTH, ENEMYBULLET_HEIGHT);
+}
+
+/* EnemyBullets Updating Routine (called at each frame) */
+const updateEnemyBullets = () => {
+    for (let index = 0; index < enemyBulletsX.length; index++) {
+        if (enemyBulletsX[index] < -ENEMYBULLET_WIDTH) {
+            enemyBulletsX.splice(index, 1);
+            enemyBulletsY.splice(index, 1);
+        }
+    }
+    for (let index = 0; index < enemyBulletsX.length; index++) {
+        drawEnemyBullet(enemyBulletsX[index], enemyBulletsY[index]);
+        enemyBulletsX[index] -= 6;
+    }
+}
+
+/* EnemyBullets Spawning Routine (called at each frame) */
+const spawnEnemyBullets = () => {
+    for (let index = 0; index < enemiesX.length; index++) {
+        if (Date.now() - lastEnemyBullets[index] > 1000) {
+            enemyBulletsX.push(enemiesX[index] - ENEMYBULLET_WIDTH);
+            enemyBulletsY.push(enemiesY[index] + ENEMY_HEIGHT / 2);
+            lastEnemyBullets[index] = Date.now();
+        }
+    }
+}
+
+/* Score Updating Routine (called at each frame) */
+const updateScore = () => {
+    ctx.font = '12px "Arial"';
+    ctx.fillStyle = "#eeeeee";
+    ctx.textAlign = "right";
+    ctx.textBaseline = "top";
+    ctx.fillText("SCORE", CANVAS_WIDTH - 30, 30);
+    ctx.font = '25px "Press Start 2P"';
+    ctx.fillStyle = "#ffffff";
+    ctx.textAlign = "right";
+    ctx.textBaseline = "top";
+    if (remainingScoreBlinks > 0) {
+        if (Date.now() - lastScoreBlink > 500) {
+            if (isScoreVisible) {
+                isScoreVisible = false;
+            } else {
+                ctx.fillText(score, CANVAS_WIDTH - 30, 50);
+                isScoreVisible = true;
+            }
+            remainingScoreBlinks--;
+            lastScoreBlink = Date.now();
+        } else {
+            if (isScoreVisible) {
+                ctx.fillText(score, CANVAS_WIDTH - 30, 50);
+            }
+        }
+    } else {
+        ctx.fillText(score, CANVAS_WIDTH - 30, 50);
+    }
+    if (score % 1000 == 0) {
+        new Audio(SCORE_SFX_PATH).play();
+        remainingScoreBlinks = 6;
+    }
+}
+
+/* Main Updating Routine (called at each frame) */
+const update = () => {
+    drawBackground();
+
+    if (isRunning) {
+        spawnAsteroid();
+        updateAsteroids();
+
+        spawnEnemy();
+        updateEnemies();
+
+        updateExplosions();
+
+        updatePlayerBullets();
+        checkForPlayerBulletCollision();
+
+        spawnEnemyBullets();
+        updateEnemyBullets();
+
+        if (isPlayerAlive) {
+            drawPlayer();
+            checkForPlayerCollision();
+            score++;
+        }
+
+        updateScore();
+    }
+
+    requestAnimationFrame(update)
+}
+
+/* Player Position Updating Routine */
+const updatePosition = () => {
+    if (!isPlayerAlive) {
+        return;
+    }
+    if (keys.includes("ArrowUp")) {
+        y -= 2;
+    }
+    if (keys.includes("ArrowDown")) {
+        y += 2;
+    }
+    if (keys.includes("Enter")) {
+        spawnPlayerBullet();
+    }
+}
+
+/* Start Game Button Handler */
+const startGame = () => {
+    const startOverlay = document.getElementById('start-overlay');
+    startOverlay.style.display = 'none';
+    const audio = new Audio(SOUNDTRACK_PATH);
+    audio.loop = true;
+    audio.play();
+    isRunning = true;
+}
+
+/* Game Controls */
+document.onkeydown = (event) => {
+    if (!keys.includes(event.key)) {
+        keys.push(event.key);
+    }
+    updatePosition();
+}
+
+document.onkeyup = (event) => {
+    keys.splice(keys.indexOf(event.key), 1);
+    updatePosition();
+}
+
+document.onmousemove = (event) => {
+    if (!isPlayerAlive) {
+        return;
+    }
+    if (event.clientY < PLAYER_HEIGHT / 2) {
+        y = 0;
+        return;
+    }
+    if (event.clientY > CANVAS_HEIGHT - PLAYER_HEIGHT / 2) {
+        y = CANVAS_HEIGHT - PLAYER_HEIGHT;
+        return;
+    }
+    y = event.clientY - PLAYER_HEIGHT / 2;
+}
+
+document.ontouchmove = (event) => {
+    if (!isPlayerAlive) {
+        return;
+    }
+    y = event.changedTouches[0].clientY - PLAYER_HEIGHT / 2;
+}
+
+window.onload = () => requestAnimationFrame(update);
+
+canvas.onclick = spawnPlayerBullet;
