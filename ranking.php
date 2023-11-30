bb28e12c65d8516a0e5b77abb19b84c7007201b0
Luca Dello Russo <lucadellorusso5@gmail.com>
2023-11-30 14:21:38 +0100

    Modifiche Home Page

--- ranking.php
+++ ranking.php
@@ -4,47 +4,47 @@
             <h3>Classifica globale</h3>
             <h4>TOP 10</h4>
         </div>
-        <div class="ranking-content">
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="50">
+        <ul class="ranking-content">
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="50">
                 <div class="user">User #1</div>
                 <div class="score">123455</div>
-            </div>
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="100">
+            </li>
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="100">
                 <div class="user">User #2</div>
                 <div class="score">123454</div>
-            </div>
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="150">
+            </li>
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="150">
                 <div class="user">User #3</div>
                 <div class="score">123453</div>
-            </div>
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="200">
+            </li>
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="200">
                 <div class="user">User #4</div>
                 <div class="score">123452</div>
-            </div>
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="250">
+            </li>
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="250">
                 <div class="user">User #5</div>
                 <div class="score">123451</div>
-            </div>
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="300">
+            </li>
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="300">
                 <div class="user">User #6</div>
                 <div class="score">123450</div>
-            </div>
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="350">
+            </li>
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="350">
                 <div class="user">User #7</div>
                 <div class="score">12345</div>
-            </div>
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="400">
+            </li>
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="400">
                 <div class="user">User #8</div>
                 <div class="score">12344</div>
-            </div>
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="450">
+            </li>
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="450">
                 <div class="user">User #9</div>
                 <div class="score">12343</div>
-            </div>
-            <div class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="500">
+            </li>
+            <li class="rank" data-aos="fade-up" data-aos-duration="500" data-aos-delay="500">
                 <div class="user">User #10</div>
                 <div class="score">12342</div>
-            </div>
-        </div>
+            </li>
+        </ul>
     </div>
 </div>
