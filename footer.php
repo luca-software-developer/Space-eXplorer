3b65652e84e8ebfb9aa2fd2cfd7d9e80f02bc7ea
Luca Dello Russo <lucadellorusso5@gmail.com>
2024-01-03 10:10:57 +0100

    Refactoring & Ottimizzazione

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
-    <div class="main-footer">
+    <div class="footer-row">
         <img id="footer-logo" src="img/footer.png" alt="Space eXplorer" title="Space eXplorer" width="100" height="100" />
-        <div class="social-section">
+        <section class="social-section">
             <h4>Social</h4>
             <div class="social-bar">
                 <a target="_blank" href="https://www.instagram.com/spaceexplorer.official/"><i class="bi bi-instagram"></i></a>
@@ -9,8 +9,8 @@
                 <a target="_blank" href="https://www.facebook.com/people/Space-Explorer/61554632938695/"><i class="bi bi-facebook"></i></a>
                 <a target="_blank" href="https://www.youtube.com/@SpaceeXplorer-official"><i class="bi bi-youtube"></i></a>
             </div>
-        </div>
-        <div class="contacts-section">
+        </section>
+        <section class="contacts-section">
             <h4>Contatti</h4>
             <ul class="contacts-list">
                 <li><a href="mailto:p.caramia@studenti.unisa.it">p.caramia@studenti.unisa.it</a></li>
@@ -18,14 +18,16 @@
                 <li><a href="mailto:a.darienzo19@studenti.unisa.it">a.darienzo19@studenti.unisa.it</a></li>
                 <li><a href="mailto:l.dellorusso6@studenti.unisa.it">l.dellorusso6@studenti.unisa.it</a></li>
             </ul>
-        </div>
+        </section>
     </div>
-    <div class="validation-section">
-        <p>
-            <a target="_blank" href="http://jigsaw.w3.org/css-validator/check/referer">
-                <img class="valid-css" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="CSS Valido!" />
-            </a>
-        </p>
+    <div class="footer-row validation-row">
+        <section class="validation-section">
+            <p>
+                <a target="_blank" href="http://jigsaw.w3.org/css-validator/check/referer">
+                    <img class="valid-css" src="http://jigsaw.w3.org/css-validator/images/vcss" alt="CSS Valido!" />
+                </a>
+            </p>
+        </section>
     </div>
     <div class="info-footer">
         Università degli Studi di Salerno — Ingegneria Informatica — Gruppo04 <span class="copyright">&copy; Space
